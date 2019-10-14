ed424e6a20900e0de5524da84df620a730622928
allison cortez <allizetorc@gmail.com>
2019-10-14 02:43:51 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -11,7 +11,7 @@ def countdown_with_sleep(number)
   while number > 0 
     puts "#{number} SECOND(S)!"
     sleep 1
-    break if number <= 0
+    # break if number <= 0
     number -= 1
 end
 return "HAPPY NEW YEAR!"
